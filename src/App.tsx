96f04c2ded6c2ba42061b45e391b1348018f9c8d
Ryan Finnie <monstermash2008@gmail.com>
2024-09-05 09:25:55 +1200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="card">
+    <div className="flex flex-col w-full max-w-screen-lg m-0 p-8 text-center gap-y-2">
       <p>Count: {count}</p>
-      <button onClick={() => setCount((count) => count + 1)}>Click me</button>
+      <button className="bg-slate-700 rounded-3xl" onClick={() => setCount((count) => count + 1)}>Click me</button>
     </div>
   );
 }
